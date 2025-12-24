7ef0ea4fc1b97ae01b5d0cdb6f1c35b3c062d7d9
Shane Barron <shane@Shanes-MacBook-Pro.local>
2025-12-24 18:14:59 -0500

    Fix Livewire component missing properties

--- src/Livewire/OtpInput.php
+++ src/Livewire/OtpInput.php
@@ -6,6 +6,26 @@ use Livewire\Component;
 
 class OtpInput extends Component
 {
+    public string $value = '';
+    public int $length = 6;
+    public string $type = 'number';
+    public bool $autofocus = false;
+    public bool $disabled = false;
+
+    public function mount(
+        string $value = '',
+        int $length = 6,
+        string $type = 'number',
+        bool $autofocus = false,
+        bool $disabled = false
+    ): void {
+        $this->value = $value;
+        $this->length = $length;
+        $this->type = $type;
+        $this->autofocus = $autofocus;
+        $this->disabled = $disabled;
+    }
+
     public function render()
     {
         return view('sb-otp-input::livewire.otp-input');
